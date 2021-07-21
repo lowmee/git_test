f549d6da409b5b786388426298a3d7dfef3bdff2
lowmee <wokiio@126.com>
2021-07-21 12:24:12 +0800

    master

--- cv_FileStorage.cpp
+++ cv_FileStorage.cpp
@@ -2,12 +2,6 @@
 // Created by rmy on 2021/1/7.
 //
 
-
-//git2  master
-//git2  br2
-
-//git2  fix
-//git2
 #include <opencv2/core/core.hpp>
 #include <opencv2/features2d/features2d.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
@@ -30,57 +24,5 @@ int main(int argc, char **argv)
     double r;
     double r;
 
-
-
-    else cout<<"success to open file at: "<<path<<endl;
-    fsSettings["registration_method"] >> registration_method;
-    fsSettings["NDT"]["res"] >> r;
-
-
-    cout<< "registration_method = "<< registration_method << endl;
-    cout<< "r = "<< r << endl;
-
-    cv::Mat M = cv::Mat::eye(3,3,CV_32F);
-    cout<<"M" << M <<endl;
-    M.resize(2);
-    cout<<"M.resize(2)"<<M <<endl;
-    M.resize(4,4);
-    cout<<"M.resize(4,4)"<<endl<<M <<endl;
-
-    cv::Mat Z = M(cv::Rect(1,1, 2, 3));
-    cout<<"Z"<<endl<<Z <<endl;
-
-    cv::Mat R;
-    cv::Size sz(3,3);
-    cv::resize(M, R, sz);
-    cout<<"R"<<endl<<R <<endl;
-
-    cv::Mat im;
-    im = cv::imread("/home/rmy/test/OpenCV_test/2.jpg", CV_LOAD_IMAGE_UNCHANGED);
-    cv::Size size = im.size();
-    cout<<"size"<<endl<<size <<endl;
-    cv::imshow("im", im);
-    cv::waitKey(0);
-
-    cv::Size rsz(480,480);
-    cv::Mat im_resize;
-    cv::resize(im, im_resize, rsz);
-    cv::Size rsize = im_resize.size();
-    cout<<"rsize"<<endl<<rsize <<endl;
-    cv::imshow("im_resize", im_resize);
-    cv::waitKey(0);
-
-    cv::Mat im_copy;
-    im_resize.copyTo(im_copy);
-    cv::imshow("im_copy", im_copy);
-    cv::waitKey(0);
-
-    cv::Mat im_border;
-    cv::copyMakeBorder(im_resize, im_border, 200, 200, 200, 200, cv::BORDER_WRAP + cv::BORDER_ISOLATED);
-    cv::imshow("im_border", im_border);
-    cv::waitKey(0);
-
-    vector<cv::KeyPoint> vKeysCell;
-    cv::FAST(M, vKeysCell, 20, true);
 
 }
